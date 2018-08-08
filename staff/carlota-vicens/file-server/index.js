22a7ed2b6c6b023b8ac444484091b5992506d12c
Carlota <krlota.barna@gmail.com>
2018-08-08 17:33:28 +0200

    file-server

---
+++ staff/carlota-vicens/file-server/index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const fs = require('fs')
+const fileUpload = require('express-fileupload')
+const package = require('./package.json')
+
+const { argv: [, , port] } = process
+
+const app = express()
+
+app.use(fileUpload())
+
+app.get('/helloworld', (req, res) => {
+    res.send(`<html>
+    <head>
+        <title>hola mundo</title>
+    </head>
+    <body>
+        <h1>hello world!</h1>
+    </body>
+</html>`)
+})
+
+app.get('/files', (req, res) => {
+    const files = fs.readdirSync('files')
+
+    res.send(`<html>
+    <head>
+        <title>files</title>
+    </head>
+    <body>
+        <ul>
+            ${files.map(file => `<li><a href='/download?file=${file}'>${file}</a> <a href='/delete?file=${file}'> <button> delete</button> </a></li>`).join('')}
+        </ul>
+
+        <form action="/files" method="post" encType="multipart/form-data">
+            <input type="file" name="upload">
+            <button>upload</button>
+        </form>
+    </body>
+</html>`)
+
+})
+
+app.post('/files', (req, res) => {
+    const { files: { upload } } = req
+
+    upload.mv(`files/${upload.name}`, function (err) {
+        if (err)
+            return res.status(500).send(err)
+
+        res.redirect('/files')
+    });
+})
+
+app.get('/download', (req, res) => {
+    const file = req.query.file
+    var file_upload = __dirname + `/files/${file}`;
+    res.download(file_upload); // Set disposition and send it.
+
+})
+
+app.get('/delete', (req, res) => {
+    const file = req.query.file
+    fs.unlinkSync(__dirname+ `/files/${file}`, function (err) {            
+        if (err) {                                                
+            console.error(err);                                    
+        }                                                          
+       console.log('File has been Deleted');                          
+    });
+    res.redirect('/files')
+})
+
+app.listen(port, () => console.log(`${package.name} ${package.version} up and running on port ${port}`))
